8946129395ad62c407da125fbfae472f42f63b94
fujitsu <apps.selusin@gmail.com>
2019-02-23 12:52:30 +0700

    .

--- ewcfg13.php
+++ ewcfg13.php
@@ -7,7 +7,7 @@
 if (!isset($EW_RELATIVE_PATH)) $EW_RELATIVE_PATH = "";
 
 // Show SQL for debug
-define("EW_DEBUG_ENABLED", TRUE, TRUE); // TRUE to debug
+define("EW_DEBUG_ENABLED", FALSE, TRUE); // TRUE to debug
 if (EW_DEBUG_ENABLED) {
 	@ini_set("display_errors", "1"); // Display errors
 	error_reporting(E_ALL ^ E_NOTICE); // Report all errors except E_NOTICE
@@ -25,7 +25,7 @@ define("EW_CONFIG_FILE_FOLDER", EW_PROJECT_NAME, TRUE); // Config file name
 define("EW_PROJECT_ID", "{994C6BDE-6323-4115-98D0-9A87BE039368}", TRUE); // Project ID (GUID)
 $EW_RELATED_PROJECT_ID = "";
 $EW_RELATED_LANGUAGE_FOLDER = "";
-define("EW_RANDOM_KEY", 'dmkGtfa8zQyrs7Nj', TRUE); // Random key for encryption
+define("EW_RANDOM_KEY", '4cdZ3tPuh3Zu2wIC', TRUE); // Random key for encryption
 define("EW_PROJECT_STYLESHEET_FILENAME", "phpcss/accounting_prj.css", TRUE); // Project stylesheet file name
 define("EW_CHARSET", "", TRUE); // Project charset
 define("EW_EMAIL_CHARSET", EW_CHARSET, TRUE); // Email charset
